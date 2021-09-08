d5b8ce7b9fef4a2ae4bd2cf4542c1d06ff487fbe
robertrapao <robertrapalo@gmail.com>
2021-09-08 11:31:47 -0600

    agreggue servicio para usuario

--- index.js
+++ index.js
@@ -8,5 +8,20 @@ app.get('/', (req, res) => {
     res.redirect('/index.html')
 })
 
+app.get('/api/consultar-usuario', (req, res) => {
+
+    let usuario = [{
+            idUsuario: 1,
+            nombre: "Erick"
+        },
+        {
+            idUsuario: 2,
+            nombre: 'ANA'
+        }
+    ]
+
+    res.status(200).json(usuario)
+
+})
 var port = process.env.PORT || 3000;
 app.listen(port);
